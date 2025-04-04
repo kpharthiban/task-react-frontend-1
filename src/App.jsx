39192278de8b50d8fac22646dc5ef70b32a62411
Pharthiban Kumarhesan <pharthibankumarhesan@gmail.com>
2025-04-04 13:52:42 +0800

    Added HTML rendering elements for frontend

--- src/App.jsx
+++ src/App.jsx
@@ -72,10 +72,87 @@ const App = () => {
   };
 
   return (
-    <>
-      <div>
+    <div>
+      <nav className='navbar navbar-dark bg-primary'>
+        <div className='container-fluid'>
+          <a className='navbar-brand' href="#">
+            Task App (Frontend)
+          </a>
+        </div>
+      </nav>
+
+      <div className='container'>
+        <form onSubmit={handleFormSubmit}>
+          <div className='mb-3 mt-3'>
+            <label htmlFor='name' className='form-label'>
+              Task Name
+            </label>
+            <input type="text" className="form-control" id="name" name="name" onChange={handleInputChange} value={formData.name} />
+          </div>
+
+          <div className='mb-3'>
+            <label htmlFor='description' className='form-label'>
+              Task Description
+            </label>
+            <input type="text" className="form-control" id="description" name="description" onChange={handleInputChange} value={formData.description} />
+          </div>
+
+          <div className='mb-3'>
+            <label htmlFor='is_complete' className='form-label'>
+              Completed?
+            </label>
+            <input type='checkbox' id='is_complete' name='is_complete' onChange={handleInputChange} value={formData.is_complete} />
+          </div>
+
+          <div className='mb-3'>
+            <label htmlFor='date' className='form-label'>
+              Due Date
+            </label>
+            <input type="text" className="form-control" id="date" name="date" onChange={handleInputChange} value={formData.date} />
+          </div>
+
+          <button type='submit' className='btn btn-primary mb-3'>
+            {taskEditId ? "Edit Task" : "Add Task"}
+          </button>
+          {taskEditId && (
+            <button type='button' onClick={resetForm} className='btn btn-secondary'>
+              Cancel
+            </button>
+          )}
+        </form>
+
+        <table className='table table-striped table-bordered table-hover'>
+          <thead>
+            <tr>
+              <th>Task Name</th>
+              <th>Description</th>
+              <th>Completed?</th>
+              <th>Due Date</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {tasks.map((task) => (
+              <tr key={task.id}>
+                <td>{task.name}</td>
+                <td>{task.description}</td>
+                <td>{task.is_complete ? "Yes" : "No"}</td>
+                <td>{task.date}</td>
+                <td>
+                  <button onClick={() => handleEditClick(task)} className='btn btn-secondary mx-2'>
+                    Edit
+                  </button>
+                  <button onClick={() => handleDeleteTask(task.id)} className='btn btn-danger mx-2'>
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
-    </>
+    </div>
+    
   )
 }
 
