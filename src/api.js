9ac69bff0afd6557c578cede0da5a3f2e6341441
Pharthiban Kumarhesan <pharthibankumarhesan@gmail.com>
2025-04-04 16:11:12 +0800

    Resetting baseURL to localhost

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Importing axios and enabling connection to the URL/location
 const api = axios.create({
-    baseURL: "https://test-fastapi-backend-g16q.onrender.com", // Change this with http://localhost:8000 - for local testing
+    baseURL: "http://localhost:8000", // Change this with http://localhost:8000 - for local testing
 });
 
 export default api;
